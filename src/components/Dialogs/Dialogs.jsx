f63a5da12dd86ab858d8b3e85ab1235a4c3bf0ea
Vasilii <v.ionichev@gmail.com>
2023-03-26 13:34:28 +0300

    lessons 27-28 completed data moved to index.js with props

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,62 +5,12 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-  let dialogs = [
-    {
-      id: 1,
-      name: "Vasilii",
-    },
-    {
-      id: 2,
-      name: "Liudmila",
-    },
-    {
-      id: 3,
-      name: "Valentina",
-    },
-    {
-      id: 4,
-      name: "Elena",
-    },
-    {
-      id: 5,
-      name: "Denis",
-    },
-    {
-      id: 6,
-      name: "Nikolai",
-    },
-  ];
-
-  let messages = [
-    {
-      id: 1,
-      message: "Hi",
-    },
-    {
-      id: 2,
-      message: "How is your IT-Kamasutra?",
-    },
-    {
-      id: 3,
-      message: "Yo",
-    },
-    {
-      id: 4,
-      message: "Yo",
-    },
-    {
-      id: 5,
-      message: "Yo",
-    },
-  ];
-
-  let DialogsElements = dialogs.map((dialog) => (
+  let DialogsElements = props.dialogs.map((dialog) => (
     <DialogItem name={dialog.name} id={dialog.id} />
   ));
 
-  let messagesElements = messages.map((message) => (
-    <Message message={message.message} id={messages.id} />
+  let messagesElements = props.messages.map((message) => (
+    <Message message={message.message} id={props.messages.id} />
   ));
 
   return (
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,32 +2,10 @@ import React from "react";
 import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-  let posts = [
-    {
-      id: 1,
-      post: "Hi, how are you?",
-      likesCount:12,
-    },
-    {
-      id: 2,
-      post: "It's my first post",
-      likesCount:23,
-    },
-    {
-      id: 3,
-      post: "Goin outdoors",
-      likesCount:12,
-    },
-    {
-      id: 4,
-      post: "Just doin HW",
-      likesCount:23,
-    },
-      ];
-      let postsElements = posts.map ((posts) => (
-        <Post message={posts.post} likesCount={posts.likesCount} />
-      ));
+const MyPosts = (props) => {
+  let postsElements = props.posts.map((posts) => (
+    <Post message={posts.post} likesCount={posts.likesCount} />
+  ));
   return (
     <div>
       <h3 className={classes.postsHeader}>My posts</h3>
@@ -42,10 +20,7 @@ const MyPosts = () => {
           <button>Remove</button>
         </div>
       </div>
-      <div className={classes.postsBlock}>
-        {postsElements}
-    
-      </div>
+      <div className={classes.postsBlock}>{postsElements}</div>
     </div>
   );
 };
